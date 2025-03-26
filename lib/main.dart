7ceee5e3453a34f6e8e436b06afc3cffd8bcecfe
julietestu <julie.testu@esme.fr>
2025-03-26 17:44:27 +0100

    add: left widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:hydrometrie_dashboard/app.dart';
+import 'screens/stat_cards_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StatCardsScreen(),
+    );
+  }
 }
