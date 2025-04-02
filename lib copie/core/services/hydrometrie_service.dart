4d24b1679d5dd37f92f04a0824a41be42a2400cd
Mahouna VAYSSIERES <144541629+miikeydev@users.noreply.github.com>
2025-04-02 20:10:29 +0200

    {supp : lib 1

--- lib copie/core/services/hydrometrie_service.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:math';
-
-class HydrometrieService {
-  Future<double> fetchAverageFlow() async {
-    await Future.delayed(const Duration(seconds: 1));
-    return Random().nextDouble() * 100;
-  }
-}
